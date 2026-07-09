2d9ed1c7e00551e12d7f0aa7ea6beb7dc9bf932a
وليد ابو يوسف <vcol42@gmail.com>
2026-07-09 16:28:13 +0300

    قران ماستر

---
+++ app/src/main/java/com/example/quranmaster/IndexItem.kt
@@ -0,0 +1,8 @@
+package com.example.quranmaster
+
+data class IndexItem(
+    val number: Int,
+    val name: String,
+    val subInfo: String,
+    val targetPosition: Int // Position in the blockList
+)
